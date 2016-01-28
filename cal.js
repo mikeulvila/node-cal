8e3b1d35445de123eb18cb5e4b51269d505cfff9
Mike Ulvila <mikeulvila@me.com>
2016-01-28 07:43:38 -0600

    calendar working with month/year command line arguments

--- cal.js
+++ cal.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node --harmony_destructuring
 'use strict';
 
-
 const generateMonth = require('./lib/month.js');
 
-console.log(generateMonth(1, 2016));
+//console.log(generateMonth(1, 2016));
 
-//const [,,...args] = process.argv;
-
-//if (args.length === 2) {
-  //const [month, year] = args;
-//} else if (args.length === 1) {
-  //const [year] = args;
-//} else {
-  //process.exit(64);
-//}
+const [,,...args] = process.argv;
+// check for two arguments
+if (args.length === 2) {
+  // assign command line arguments to const month and year
+  const [month, year] = args;
+    // check that year is between 1753 - 9999
+    if (year >= "1753" && year <= "9999") {
+       console.log(generateMonth(parseInt(month), parseInt(year)));
+    } else {
+      console.log('Please pick a year between 1753 and 9999');
+    }
+} else if (args.length === 1) {
+  const [year] = args;
+} else if (args.length === 0) {
+  let date = new Date();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  console.log(generateMonth(month, year));
+} else {
+  process.exit(64);
+}
 
