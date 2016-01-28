b1b6096888d8c1af63048fc9e4a6fad34e6be418
Mike Ulvila <mikeulvila@me.com>
2016-01-28 10:42:48 -0600

    added lodash and .map to convert command line arguments to number instead of parseInt

--- cal.js
+++ cal.js
@@ -6,18 +6,20 @@ const generateMonth = require('./lib/month.js');
 //console.log(generateMonth(1, 2016));
 
 const [,,...args] = process.argv;
+
 // check for two arguments
 if (args.length === 2) {
-  // assign command line arguments to const month and year
-  const [month, year] = args;
+
+  // assign command line arguments to const month and year and convert string to number
+  const [month, year] = args.map(num => +num);
     // check that year is between 1753 - 9999
-    if (year >= "1753" && year <= "9999") {
-       console.log(generateMonth(parseInt(month), parseInt(year)));
+    if (year >= 1753 && year <= 9999) {
+       console.log(generateMonth(month, year));
     } else {
       console.log('Please pick a year between 1753 and 9999');
     }
 } else if (args.length === 1) {
-  const [year] = args;
+  const [year] = args.map(num => +num);
 } else if (args.length === 0) {
   let date = new Date();
   let month = date.getMonth() + 1;
